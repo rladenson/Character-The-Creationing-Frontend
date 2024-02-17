50465924ba04795cc6ff046e8396fb556e1a8d05
Petal Ladenson <habonimkef@gmail.com>
2024-02-17 01:29:59 -0700

    Initial commit, made home page, basic readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Character The Creationing
+A character builder for the joke system [Dungeons the Dragoning](https://dtd.wikidot.com)
+
+[Backend Repository](https://github.com/rladenson/Character-The-Creationing-Backend/)
+|
+[Trello](https://trello.com/b/bYBLUkyA/character-the-creationing)
+---
+
+Credits: `npm create svelte@latest`
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Nav, NavItem, NavLink, Navbar, NavbarBrand } from "@sveltestrap/sveltestrap";
+</script>
+<Navbar color="dark" theme="dark">
+    <NavbarBrand href="/">Home</NavbarBrand>
+    <Nav underline theme="dark">
+        <NavItem>
+            <NavLink href="login">Login</NavLink>
+        </NavItem>
+    </Nav>
+</Navbar>
+<slot />
