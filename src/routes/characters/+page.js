4468b59ba3848167306a450ef5dfc9d8625824bb
rladenson <habonimkef@gmail.com>
2024-03-04 12:59:30 -0700

    character index view redirects to login if logged out

--- src/routes/characters/+page.js
+++ src/routes/characters/+page.js
@@ -1,9 +1,15 @@
 import { baseUrl } from '$lib/stores.js';
+import { pingServer } from '$lib/pingServer.js';
 
 export async function load({ fetch }) {
+	const token = window.localStorage.getItem('accessToken');
+	if (!token) throw redirect(303, '/');
+	const loggedIn = await pingServer(token);
+	if (!loggedIn) throw redirect(303, '/');
+	
 	const data = await fetch(baseUrl + 'api/characters', {
 		headers: {
-			Authorization: 'Bearer ' + window.localStorage.getItem('accessToken')
+			Authorization: 'Bearer ' + token
 		}
 	});
 	return {
